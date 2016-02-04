3c33324e7f786e9ad6b22a0f598d13ef4eb6ccdc
Henry Pitcairn <735tesla@gmail.com>
2016-02-04 12:24:28 -0500

    stuff

--- Python/commander.py
+++ Python/commander.py
@@ -9,48 +9,38 @@ Foxtrot (C) 2016 <foxtrotnull@gmail.com>
 import ConfigParser
 import os
 import sys
-import socket
+from bufferedsocket import BufferedSocket
 import string
 import time
 
-class Commander:
-	print "[*] WiFi Pineapple Commander"
-	print "[*] Looking for Commander.conf..."
-	if os.path.exists('commander.conf'):
-		print "[*] Found configuration file!"
-		print " "
-		config = ConfigParser.RawConfigParser()
-		config.read('commander.conf')
-		if config.has_section('Network') & config.has_section('Commands'):
-			print "[*] Valid configuration file... proceeding"
+class Commander(object):
+	def __init__(self):
+		print "[*] WiFi Pineapple Commander"
+		print "[*] Looking for Commander.conf..."
+		if os.path.exists('commander.conf'):
+			print "[*] Found configuration file!"
 			print " "
+			self.config = ConfigParser.RawConfigParser()
+			self.config.read('commander.conf')
+			if self.config.has_section('Network') & self.config.has_section('Commands'):
+				print "[*] Valid configuration file... proceeding"
+				print " "
+			else:
+				print "[!] Configuration does not have Network or Command blocks."
+				sys.exit(1)
 		else:
-			print "[!] Configuration does not have Network or Command blocks."
+			print "[!] Could not find configuration file! Exiting..."
 			sys.exit(1)
-	else:
-		print "[!] Could not find configuration file! Exiting..."
-		sys.exit(1)
 
-
-class Commands(Commander):
-	def availableCommands():
-		print "[*] Listing commands..."
-		
-		commands = Commander.config.options('Commands')
-		for command in commands:
-			print "    " + command
-
-		print " "
-
-	availableCommands()
-
-
-class Client(Commander, Commands):
-	def connect():		
-		server = Commander.config.get('Network', 'Server')
-		port = Commander.config.getint('Network', 'Port')
-		nickname = Commander.config.get('Network', 'Nickname')
-		channel = Commander.config.get('Network', 'Channel')
+class Client(Commander):
+	def __init__(self):
+		super(Client, self).__init__()
+		self.sock = BufferedSocket()
+	def connect(self):		
+		server = self.config.get('Network', 'Server')
+		port = self.config.getint('Network', 'Port')
+		nickname = self.config.get('Network', 'Nickname')
+		channel = self.config.get('Network', 'Channel')
 
 		print "[*] Using these connection settings! :"
 		print "    Server: " + server
@@ -59,30 +49,37 @@ class Client(Commander, Commands):
 		print "    Channel: " + channel
 		print " "
 
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		print "[*] Connecting!"
-		sock.connect((server, port))
+		self.sock.connect(server, port)
 
 		print "[*] Sending nickname and joining " + channel
-		sock.send("NICK %s\r\n" % nickname)
-		sock.send("USER %s 8 * :%s\r\n" % (nickname, nickname))
+		self.sock.sendline("NICK %s" % nickname)
+		self.sock.sendline("USER %s 8 * :%s" % (nickname, nickname))
 		time.sleep(5)
-		sock.send("JOIN %s\r\n" % channel)
-		sock.send("PRIVMSG %s :Connected!")
+		self.sock.sendline("JOIN %s" % channel)
+		self.sock.sendline("PRIVMSG %s :Connected!" % channel)
 		print "[*] Connected!"
 		print " "
-
 		while True:
-			buff = sock.recv(2048)
-
-			if buff.find('PING') != -1:
+			line = self.sock.nextline()
+			print repr(line)
+			if line.startswith('PING'):
 				print "[*] Replying to ping from server"
-				sock.send('PONG ' + buff.split() [1] + '\r\n')
+				self.sock.sendline('PONG %s' % line.split()[1])
 
-			commands = Commander.config.options('Commands')
-			for command in commands:
-				if buff.find(command) != -1:
-					print "[*] Found command %s" % command
-			
+			commands = self.config.options('Commands')
+			linearr = line.split(' ')
+			if len(linearr) >= 4and linearr[3] == 'PRIVMSG':
+				message = 'PRIVMSG '.join(line.split('PRIVMSG ')[1:])
+				message = ' '.join(message.split(' ')[1:])[1:]
+				print '\n'+message+'\n'
+				for command in commands:
+					if message.startswith(command):
+						print "[*] Found command %s" % command
 
-	connect()
+def main():
+	client = Client()
+	client.connect()
+
+if __name__ == '__main__':
+	main()
